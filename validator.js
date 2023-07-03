d272676322c29c4e49a8e526d10e932c116e4301
Suman Rizvi <sumanrizvi@gmail.com>
2023-07-03 15:55:31 -0700

    feat: added testing

--- validator.js
+++ validator.js
@@ -3,7 +3,7 @@ function isInvalidEmail(userObject) {
 }
 
 function isEmptyPayload(userObject) {
-    return Object.keys(userObject).lenght === 0
+    return Object.keys(userObject).length === 0
 }
 
 module.exports = {
